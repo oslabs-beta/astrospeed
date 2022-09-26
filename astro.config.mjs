38d673cc5ad4a278c39d61305b1d7ba45049d942
Luke McInerney <mciluke@gmail.com>
2022-09-25 21:38:11 -0400

    updated configs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,12 @@
 import { defineConfig } from 'astro/config';
+import astroSingleFile from 'astro-single-file';
+
 
 // https://astro.build/config
 export default defineConfig({
-  outDir: '../../astrospeed'
+  integrations: [astroSingleFile()],
+  // outDir: '../../astrospeed'
+  // site: './'
+  // build.assetsDir: 'assetsa'
 });
 
