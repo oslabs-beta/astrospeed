d09148eca5fb742883a8a8853bca97eefa3adfa9
mgon123 <106345774+mgon123@users.noreply.github.com>
2022-09-29 12:22:56 -0400

    fixed circular gages

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import * as React from 'react';
 import lhr from '../lighthouse.json';
 import ListContainer from "../src/components/ListContainer/ListContainer.jsx" 
 import LineChart from "./components/LineChart.jsx";
-// import DialChart from "./components/DialChart.jsx";
+import DialChart from "./components/DialChart.jsx";
 
 
 interface Props {
@@ -20,29 +20,17 @@ class App extends React.Component<Props> {
     const currSeo = lhr[lhr.length-1].categories.seo.score * 100;
     const gapSeo = 100 - currSeo;
 
+    const divStyle = {
+      display:'flex'
+    }
+
     return (
       <>
-        {/* <h1>
-          Hello {name}
-        </h1> */}
         Report generated at: {reportDate.toLocaleString()} <br />
-        {/* Performance: {lhr[lhr.length-1].categories.performance.score}  <br />
-        Search Engine Optimization (SEO): {lhr[lhr.length-1].categories.seo.score}  <br /> */}
-        {/* Put this centre of dial and legend -- Performance: {`${currPerf}%`}  <br />
-        Put this centre of dial  and legend -- Search Engine Optimization (SEO): {`${currSeo}%`}  <br /> */}
 
-        {/* put the dials side by side */}
-        {/* <div className='dialsRow'> */}
-        
-        <div id="chart">
-          {/* <DialChart 
-            name={'Current Performance'}
-            results={[currPerf, gapPerf]}
-          />
-          <DialChart 
-          name={'Current SEO'}
-          results={[currSeo, gapSeo]}
-          /> */}
+        <div style={divStyle}>
+            <DialChart name={'Current Performance'} data = {currPerf}/>
+            <DialChart name={'Current SEO'} data = {currSeo}/>
         </div>
 
         <LineChart />
--- src/components/DialChart.jsx
+++ src/components/DialChart.jsx
@@ -2,20 +2,27 @@ import React from "react";
 import ReactApexChart from "react-apexcharts";
 
 
-class ApexChart extends React.Component {
+class DialChart extends React.Component {
     constructor(props) {
         super(props);
 
         this.state = {
-
-            series: [76],
+            series: [props.data],
             options: {
+                //Removed Title because I like to use the Label better for single data series
+                // title : {
+                //     text: props.name,
+                //     align: 'center'
+                // }, 
+                // title: this.props.name,
                 chart: {
                     type: 'radialBar',
-                    offsetY: -20,
-                    sparkline: {
-                        enabled: true
-                    }
+                    height: '100px',
+                    width: '100px'
+                    // offsetY: -20,
+                    // sparkline: {
+                    //     enabled: true
+                    // }
                 },
                 plotOptions: {
                     radialBar: {
@@ -23,24 +30,25 @@ class ApexChart extends React.Component {
                         endAngle: 90,
                         track: {
                             background: "#e7e7e7",
-                            strokeWidth: '97%',
-                            margin: 5, // margin is in pixels
+                            // strokeWidth: '97%', this is not an option in radialBar
+                            // margin: 5, // margin is in pixels removed because only 1 track
                             dropShadow: {
                                 enabled: true,
                                 top: 2,
                                 left: 0,
-                                color: '#999',
+                                // color: '#999', no color option here
                                 opacity: 1,
                                 blur: 2
                             }
                         },
                         dataLabels: {
                             name: {
-                                show: false
+                                show: true
                             },
                             value: {
-                                offsetY: -2,
-                                fontSize: '22px'
+                                offsetY: -50,
+                                fontSize: '22px',
+                                formatter: (val) => val
                             }
                         }
                     }
@@ -58,10 +66,10 @@ class ApexChart extends React.Component {
                         inverseColors: false,
                         opacityFrom: 1,
                         opacityTo: 1,
-                        stops: [0, 50, 53, 91]
+                        stops: [0, 50, 53, 91] // what is up with these stops
                     },
                 },
-                labels: ['Average Results'],
+                labels: [props.name],
             },
 
 
@@ -69,164 +77,16 @@ class ApexChart extends React.Component {
     }
 
     render() {
+        const divStyle = {
+            width: '50%',
+        }
         return (
-            <div id="chart">
+            <div style={divStyle}>
                 <ReactApexChart options={this.state.options} series={this.state.series} type="radialBar" />
             </div>
         );
     }
 }
 
-const domContainer = document.querySelector('#app');
-ReactDOM.render(React.createElement(ApexChart), domContainer);
-
-// export default DialChart;
-
-//   var chart = new ApexCharts(document.querySelector("#app"), options);
-  
-//   chart.render();
-
-
-
-/////////////////////the below works but is a donut graph that is causing overlap issues with the report
-
-// import React from "react";
-// import ReactApexChart from "react-apexcharts";
-// // import lhr from '../../lighthouse.json'
-
-// class DialChart extends React.Component {
-//     constructor(props) {
-//       super(props);
-
-//       this.state = {
-      
-//         series: this.props.results,
-//         // chartOptions: {
-//         //     labels: this.props.name
-//         // },
-//         options: {
-//           chart: {
-//             width: 380,
-//             type: 'donut',
-//           },
-//           //set scalling of the dial
-//           plotOptions: {
-//             pie: {
-//                 expandOnClick: false,
-//                 customScale: 0.9,
-//                 donut: {
-//                     size: '90%',
-//                     labels: {
-//                       show: true,
-//                       name: {
-//                         show: false,
-//                       },
-//                       value: {
-//                         show: true,
-//                         fontSize: '5vh',
-//                         // fontFamily: 'custom',
-//                         // fontWeight: 'lighter',
-//                         color: "#000000"
-//                       },
-//                       total: {
-//                         show: false,
-//                         showAlways: true,
-//                         label: 'Total',
-//                         // fontSize: '1px',
-//                         // fontFamily: 'Comic Sans',
-//                         // fontWeight: 'lighter',
-//                         color: '#000000'
-//                       }
-//                     }
-//                   }
-//             }
-//           },
-//           dataLabels: {
-//             enabled: false
-//           },
-//           fill: {
-//             type: 'gradient',
-//             gradient: {
-//               shade: 'light',
-//               shadeIntensity: 0.5,
-//               gradientToColors: undefined,
-//               inverseColors: true,
-//               opacityFrom: 0.8,
-//               OpacityTo: 0,
-//               stop: []
-//             },
-//             colors: ["#3700A4", "#ffffff"]
-//           },
-//           legend: {
-//             show: false,
-//           },
-//           title: {
-//             // text: this.props.name,
-//           },
-//           responsive: [{
-//             breakpoint: 480,
-//             options: {
-//               chart: {
-//                 width: 100
-//               },
-//               legend: {
-//                 show: false,
-//               }
-//             }
-//           }]
-//         },
-//       };
-//     }
-
-//     render() {
-//         const divStyle = {
-//             padding: '10px',
-//             float: 'left',
-//             height: '20vh',
-//             minHeight: '100px',
-//         }
-//         const tableStyle = {
-//             textAlign: 'center',
-//         }
-
-//         const parentStyle = {
-//             position: 'absolute',
-//         }
-//         const childStyle = {
-//             position: 'absolute',
-//             top: '35%',
-//             left: '42%'
-//         }
-
-//         console.log('this is donut props passed in fron the App, ', this.props.results)
-//         return (
-//             <div style={divStyle}>
-//                 <table style={tableStyle}>
-//                     <tr>
-//                         <th>{this.props.name}</th>
-//                     </tr>
-//                     <div style={parentStyle}>
-//                         <tr>
-//                             <td>
-//                                 <div style={childStyle}>
-//                                     {this.props.results[0]}%
-//                                 </div>
-//                                 <ReactApexChart
-//                                     options={this.state.options}
-//                                     series={this.state.series}
-//                                     type="donut"
-//                                     width="50%"
-//                                 />
-//                                 on this commit
-//                             </td>
-//                         </tr>
-//                     </div>
-//                 </table>
-//             </div>
-//         );
-//     }
-//   }
-
-//   export default DialChart;
-
+export default DialChart; 
 
