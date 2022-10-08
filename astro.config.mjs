9b6d6ed070f1276c34e8b05e9243f2a82c9ca6f3
mgon123 <106345774+mgon123@users.noreply.github.com>
2022-10-08 13:44:36 -0400

    Clean up website files

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,5 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-	//CHANGE THIS TO SERVER BEFORE DEPLOYING!!!!
-	// output:'static',
 	integrations: [tailwind()],
 });
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,12 +1,4 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-		// colors: {
-		// 	'black':'#000000',
-		// 	'purple':'#3700A4',
-		// 	'yellow' : '#E4E684'
-		// }
-	},
 	plugins: [],
 };
